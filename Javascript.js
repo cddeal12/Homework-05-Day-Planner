026b12e4335b12fc19eef5bca8f5e0bac1c49166
Nemancer <cddeal12@gmail.com>
2020-09-12 17:04:31 -0700

    Completed localstorage for each field

--- Javascript.js
+++ Javascript.js
@@ -29,8 +29,23 @@ for (i=0; i<nineToFive.length; i++) {
  };
 
     // Load any saved text from storage and apply it to the appropriate block of time
-
+nineToFive[0].value = localStorage.getItem("0");
+nineToFive[1].value = localStorage.getItem("1");
+nineToFive[2].value = localStorage.getItem("2");
+nineToFive[3].value = localStorage.getItem("3");
+nineToFive[4].value = localStorage.getItem("4");
+nineToFive[5].value = localStorage.getItem("5");
+nineToFive[6].value = localStorage.getItem("6");
+nineToFive[7].value = localStorage.getItem("7");
+nineToFive[8].value = localStorage.getItem("8");
 
 
 // When the save button is clicked...
     // Save the entered text from the appropriate field, to be reapplied to the field on re-visit
+document.querySelector(".container").addEventListener("click", function (event) {
+    console.log("you clicked the container")
+    if (event.target.matches(".saveBtn")) {
+        btnId = event.target.id;
+        localStorage.setItem(btnId, nineToFive[btnId].value);
+    }
+});
